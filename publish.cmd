eb91838b88d9a42d5907cf846ef5074979684a34
Rick van Dam <Rick.van.Dam@avivasolutions.nl>
2018-03-16 12:06:11 +0100

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=knowledgesessionbot-Web-Deploy.pubxml -p:Password=J2KjnBzu18b1cDQSy29KmNo9F0YoKZBJJw6xiwWwggMfvnu1rLKsQnxASto6
+
